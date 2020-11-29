6189b8ceb8c634d36c1528b0b86384a5797d0305
Alan Silva <abs10@globomail.com>
2020-11-29 10:45:41 -0300

    Initial Commit

---
+++ NewsAppRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAppRxSwift
+//
+//  Created by Alan Silva on 29/11/20.
+//  Copyright © 2020 Alan Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
